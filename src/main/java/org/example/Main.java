f580bd270670a3c61fd16af076418808781e7057
Samuel_Alvarez <s.alvarez05@ufromail.cl>
2023-04-05 10:26:05 -0400

    Creación  de rama y metodo equivalente del paso 2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,30 @@
 package org.example;
+import java.util.Scanner;
 
 public class Main {
+    public static void main(String[] args) {
+        String palabra;
+        Scanner teclado = new Scanner(System.in);
+        palabra = teclado.next();
+        if (esPalindromo(palabra)==true){
+            System.out.println("la palabra es un palindromo");
+        }else{
+            System.out.println("La palabra no es un palindromo");
+        }
+
+
+    }
+
+    private static boolean esPalindromo(String palabra) {
+
+        String resultado="";
+        for (int i = 0; i < palabra.length(); i++) {
+            resultado+=palabra.charAt(palabra.length()-1-i);
+        }
+        if(resultado.equals(palabra)){
+            return true;
+        }
+        return false;
+    }
 
 }
